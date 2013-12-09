11bafef3622854c7e889e019301901687088b230
CILT Support <ciltsupport@CILTs-iMac-2.local>
2013-12-08 19:47:29 -0600

    altered nonav

--- project/js/header.js
+++ project/js/header.js
@@ -9,7 +9,12 @@ document.write(
 '        <span class="icon-bar"></span>' +
 '      </button>' +
 '      <a href="index.html" class="navbar-brand">Campus Connection</a>' +
-'    </div>' +
+'    </div>'
+);
+
+// see if we want to show the nav on this page
+if (typeof $nonav === 'undefined' || !$nonav) {
+    document.write(
 '    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">' +
 '      <ul class="nav navbar-nav">' +
 '        <li><a href="schedule.html">Schedule</a></li>' +
@@ -19,7 +24,11 @@ document.write(
 '        <li><a href="info.html">Info</a></li>' +
 '        <li><a href="index.html">Logout</a></li>' +
 '      </ul>' +
-'    </nav>' +
+'    </nav>'
+    );
+}
+
+document.write(
 '  </div>' +
 '</header>'
 );
