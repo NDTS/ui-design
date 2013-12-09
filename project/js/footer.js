9521476a345c7a4d009b1ece1e944842eda94580
Michael Hlas <mhlas7@gmail.com>
2013-12-08 20:51:01 -0600

    Added field checks for account claim

--- project/js/footer.js
+++ project/js/footer.js
@@ -7,23 +7,23 @@ document.write(
 '</footer>'
 );
 
-$('a, button.btn-block').click(function() {
-	$('body').css('cursor', 'wait');
+$("a, button.btn-block").click(function() {
+	$("body").css("cursor", "wait");
     $("#overlay").fadeIn(250);
 	if ($(this).is('a')) {
-		var href = $(this).attr('href');
+		$href = $(this).attr('href');
 	    setTimeout(function() {
 		    window.location = href;
 	    }, 1500);
     } else if ($(this).html() == "Login") {
         setTimeout(function() {
             $("#overlay").fadeOut(250);
-            $('body').css('cursor', 'auto');
+            $('body').css('cursor', "auto");
             $logins = {
-                'marshall':'mattingly',
-                'michael':'hlas',
-                'zachary':'maguire',
-                'stephen':'miller'
+                "marshall":"mattingly",
+                "michael":"hlas",
+                "zachary":"maguire",
+                "stephen":"miller"
             };
 
             $username = $("#usernameInput").val();
@@ -34,7 +34,7 @@ $('a, button.btn-block').click(function() {
                 window.location = "schedule.html";
             } else {
                 // show an error message, if needed
-                $("#ucMessage").html('Username or password incorrect. Try again or click the <a href="forgot.html">Forgot your password?</a> link.');
+                $("#ucMessage").html('Username or password incorrect. Try again or click the <a href="forgot.html">Forgot your password</a> link.');
                 if ($("#ucMessage").is(":visible")) {
                     $("#ucMessage").hide();
                 }
@@ -53,7 +53,59 @@ $('a, button.btn-block').click(function() {
                 }
             }
         }, 1500);
-    }
+    } else if ($(this).html() == "Register") {
+		$("#ucMessage").fadeOut(400);
+		setTimeout(function() {
+			$("#overlay").fadeOut(250);
+            $("body").css("cursor", "auto");
+			
+			$idInput = $("#idInput").val();
+			$usernameInput = $("#usernameInput").val();
+			$passwordInput = $("#passwordInput").val();
+			$confirmPasswordInput = $("#passwordConfirmInput").val();
+			$("#ucMessage").removeClass("alert alert-success alert-info alert-warning alert-danger").addClass("alert").hide();
+			
+			if ($idInput && $usernameInput && $passwordInput && $confirmPasswordInput && $passwordInput === $confirmPasswordInput) {	
+				$("#ucMessage").html("You have claimed your account! An email will be sent to verify your email address").addClass("alert-success");
+			} else {
+				$idParent = $("#idInput").parent().parent(".form-group");
+				$usernameParent = $("#usernameInput").parent().parent(".form-group");
+                $passwordParent = $("#passwordInput").parent().parent(".form-group");
+				$passwordConfirmParent = $("#passwordConfirmInput").parent().parent(".form-group");
+				$idParent.removeClass("has-warning has-error");
+				$usernameParent.removeClass("has-warning has-error");
+                $passwordParent.removeClass("has-warning has-error");
+				$passwordConfirmParent.removeClass("has-warning has-error");
+				
+				$errors = "";
+				if (!$idInput) {
+					$errors += ", id number";
+					$idParent.addClass("has-error");
+				}
+				if (!$usernameInput) {
+					$errors += ", username";
+					$usernameParent.addClass("has-error");
+				}
+				if (!$passwordInput) {
+					$errors += ", password";
+					$passwordParent.addClass("has-error");
+				}
+				if ($errors) {
+					$errors = "You must enter your" + $errors;
+				}
+				if ($passwordInput !== $confirmPasswordInput) {
+					if ($errors) {
+						$errors += "<br />";
+					}
+					$errors += "Your passwords do not match";
+					$passwordParent.addClass("has-error");
+					$passwordConfirmParent.addClass("has-error");
+				}
+				$("#ucMessage").html($errors).addClass("alert-danger");
+			}
+			$("#ucMessage").fadeIn(400);
+		}, 1500);
+	}
 
 	return false;
 });
